0c91df847f5f84fa09cc6d0eab31371122836817
Alex <alex-sc@users.noreply.github.com>
2024-12-11 14:01:39 +0200

    sign.js fixes

--- sign.js
+++ sign.js
@@ -1,7 +1,11 @@
 exports.default = async function(configuration) {
-    const pemPath = __dirname;
+    const DIGICERT_API_KEY = process.env.DIGICERT_API_KEY;
+    const DIGICERT_CERT_ALIAS = process.env.DIGICERT_CERT_ALIAS;
+    const DIGICERT_CLIENT_CERT_PATH = process.env.DIGICERT_CLIENT_CERT_PATH;
+    const DIGICERT_CLIENT_CERT_PASSWORD = process.env.DIGICERT_CLIENT_CERT_PASSWORD;
+
     require("child_process").execSync(
-        `jsign --storetype DIGICERTONE --storepass "<api-key>|/path/to/Certificate_pkcs12.p12|<password>" --alias test \"${configuration.path}\"`,
+        `java -jar jsign-6.0.jar --storetype DIGICERTONE --storepass "${DIGICERT_API_KEY}|${DIGICERT_CLIENT_CERT_PATH}|${DIGICERT_CLIENT_CERT_PASSWORD}" --alias ${DIGICERT_CERT_ALIAS} \"${configuration.path}\"`,
         {
             stdio: "inherit"
         }
